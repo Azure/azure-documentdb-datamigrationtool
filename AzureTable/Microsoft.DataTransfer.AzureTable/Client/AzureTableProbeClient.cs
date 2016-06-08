1668fa2e342103d4d0b6dba4605e01a902402d18
Arvind Narasimhan <arvind2205@gmail.com>
2016-06-07 17:36:23 -0700

    This file was reverted somehow in the previous changelist. Adding back in

--- AzureTable/Microsoft.DataTransfer.AzureTable/Client/AzureTableProbeClient.cs
+++ AzureTable/Microsoft.DataTransfer.AzureTable/Client/AzureTableProbeClient.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.WindowsAzure.Storage;
+﻿using Microsoft.DataTransfer.AzureTable.Source;
+using Microsoft.DataTransfer.AzureTable.Wpf.Shared;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Threading.Tasks;
 
@@ -14,14 +18,33 @@ namespace Microsoft.DataTransfer.AzureTable.Client
         /// </summary>
         /// <param name="connectionString">Azure Table storage connection string to use to connect to the account.</param>
         /// <returns>Task that represents asynchronous connection operation.</returns>
-        public async Task TestConnection(string connectionString)
+        public async Task TestConnection(AzureTableProbeClientParameter azureTableProbeClientParameter)
         {
-            if (String.IsNullOrEmpty(connectionString))
+            if (String.IsNullOrEmpty(azureTableProbeClientParameter.ConnectionString))
                 throw Errors.ConnectionStringMissing();
 
-            var properties = await CloudStorageAccount.Parse(connectionString).CreateCloudTableClient().GetServicePropertiesAsync();
+            CloudStorageAccount account = CloudStorageAccount.Parse(azureTableProbeClientParameter.ConnectionString);
+            CloudTableClient client = account.CreateCloudTableClient();
+
+            var properties = await client.GetServicePropertiesAsync();
             if (properties == null)
+            {
                 throw Errors.EmptyResponseReceived();
+            }
+
+           /// If a secondary specific setting has been requested, make sure that they have a secondary configured. 
+           if (azureTableProbeClientParameter.LocationMode != null && azureTableProbeClientParameter.LocationMode != AzureTableLocationMode.PrimaryOnly)
+           {
+               try
+               {
+                   client.DefaultRequestOptions.LocationMode = LocationMode.SecondaryOnly;
+                   var props = await client.GetServicePropertiesAsync();
+               }
+               catch (StorageException e)
+               {
+                   throw Errors.SecondaryNotDefined(e.Message);
+               }
+           }
         }
     }
 }
