968650a7e7283180fc70d4932d0d0d9855d15b77
Mike Rousos <MikeRou@Microsoft.com>
2018-06-05 10:10:01 -0400

    Add inital FirebaseJsonFile project

---
+++ FirebaseJsonFile/Microsoft.DataTransfer.FirebaseJsonFile/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.DataTransfer.FirebaseJsonFile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Microsoft.DataTransfer.FirebaseJsonFile")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("236801a6-4ef6-42c6-8e1a-4ef4f1896e07")]
