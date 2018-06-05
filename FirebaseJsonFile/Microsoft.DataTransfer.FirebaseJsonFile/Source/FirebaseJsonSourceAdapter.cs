e7976a9e038e42e3b54c310c387b8184c1a13c4e
Mike Rousos <MikeRou@Microsoft.com>
2018-06-05 11:31:05 -0400

    Implement FirebaseJsonFileSourceAdapterFactory and register with DI

--- FirebaseJsonFile/Microsoft.DataTransfer.FirebaseJsonFile/Source/FirebaseJsonSourceAdapter.cs
+++ FirebaseJsonFile/Microsoft.DataTransfer.FirebaseJsonFile/Source/FirebaseJsonSourceAdapter.cs
@@ -2,11 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.DataTransfer.Basics;
+using Microsoft.DataTransfer.Basics.Files.Source;
+using Microsoft.DataTransfer.Extensibility;
+using Newtonsoft.Json;
 
 namespace Microsoft.DataTransfer.FirebaseJsonFile.Source
 {
-    sealed class FirebaseJsonFileSourceAdapter
+    sealed class FirebaseJsonFileSourceAdapter: IDataSourceAdapter
     {
+        private readonly ISourceStreamProvider sourceStreamProvider;
+        private readonly string node;
+        private readonly string idField;
+        private readonly string collectionField;
+        private readonly JsonSerializer serializer;
+
+        public FirebaseJsonFileSourceAdapter(ISourceStreamProvider sourceStreamProvider, string node, string idField, string collectionField, JsonSerializer serializer)
+        {
+            Guard.NotNull("sourceStreamProvider", sourceStreamProvider);
+            Guard.NotNull("serializer", serializer);
+
+            this.sourceStreamProvider = sourceStreamProvider;
+            this.node = node;
+            this.idField = idField;
+            this.collectionField = collectionField;
+            this.serializer = serializer;
+        }
+
+        public Task<IDataItem> ReadNextAsync(ReadOutputByRef readOutput, CancellationToken cancellation)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            
+        }
     }
 }
---
+++ FirebaseJsonFile/Microsoft.DataTransfer.FirebaseJsonFile/Source/IFirebaseJsonFileSourceAdapterConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.DataTransfer.FirebaseJsonFile.Source
+{
+    /// <summary>
+    /// Contains configuration for Firebase JSON files data source adapter. 
+    /// </summary>
+    public interface IFirebaseJsonFileSourceAdapterConfiguration
+    {
+        /// <summary>
+        /// Gets the list of file names to read JSON data from.
+        /// </summary>
+        [Display(ResourceType = typeof(ConfigurationResources), Description = "Source_Files")]
+        IEnumerable<string> Files { get; }
+
+        /// <summary>
+        /// Gets the value that indicates whether input files should be decompressed.
+        /// </summary>
+        [Display(ResourceType = typeof(ConfigurationResources), Description = "Source_Decompress")]
+        bool Decompress { get; }
+
+        /// <summary>
+        /// Gets an optional value indicating the node path to import from the source files
+        /// </summary>
+        [Display(ResourceType = typeof(ConfigurationResources), Description = "Source_Node")]
+        string Node { get; }
+
+        /// <summary>
+        /// Gets the field name to be used for storing the Firebase ID
+        /// </summary>
+        [Display(ResourceType = typeof(ConfigurationResources), Description = "Source_IdField")]
+        string IdField { get; }
+
+        /// <summary>
+        /// Gets the field name to be used for storing the Firebase top-level node name (to be interpreted as a collection)
+        /// </summary>
+        [Display(ResourceType = typeof(ConfigurationResources), Description = "Source_CollectionField")]
+        string CollectionField { get; }
+    }
+}
