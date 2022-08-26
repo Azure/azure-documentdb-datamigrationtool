bff0dfc2ef7a957eb6058dafe234b4299bc23335
Chris Pietschmann <chris@build5nines.com>
2022-08-26 15:33:49 -0400

    Start working on AzureTableAPI extension

---
+++ Extensions/AzureTableAPI/Microsoft.DataTransfer.AzureTableAPIExtension/AzureTableAPIDataSourceExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Microsoft.DataTransfer.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.DataTransfer.AzureTableAPIExtension
+{
+    [Export(typeof(IDataSourceExtension))]
+    public class AzureTableAPIDataSourceExtension : IDataSourceExtension
+    {
+        public string DisplayName => "JSON";
+        public async IAsyncEnumerable<IDataItem> ReadAsync(IConfiguration config, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var settings = config.Get<JsonSourceSettings>();
+            settings.Validate();
+
+            if (settings.FilePath != null)
+            {
+                await using var file = File.OpenRead(settings.FilePath);
+                var list = await JsonSerializer.DeserializeAsync<List<Dictionary<string, object?>>>(file, cancellationToken: cancellationToken);
+
+                if (list != null)
+                {
+                    foreach (var listItem in list)
+                    {
+                        yield return new JsonDictionaryDataItem(listItem);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Extensions/AzureTableAPI/Microsoft.DataTransfer.AzureTableAPIExtension/AzureTableAPISourceSettings.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.DataTransfer.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.DataTransfer.AzureTableAPIExtension
+{
+    public class AzureTableAPISourceSettings : IDataExtensionSettings
+    {
+        [Required]
+        public string? FilePath { get; set; }
+    }
+}
