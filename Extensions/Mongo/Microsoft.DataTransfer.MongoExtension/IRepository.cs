5e6479c5964c33fc877808c10f286c5ea15f6589
Abraham Hosch <abraham.hosch@solliance.net>
2022-08-29 08:51:19 -0500

    Clean up file

---
+++ Extensions/Mongo/Microsoft.DataTransfer.MongoExtension/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Microsoft.DataTransfer.MongoExtension;
+
+public interface IRepository<TDocument>
+{
+    ValueTask Add(TDocument item);
+    ValueTask AddRange(IEnumerable<TDocument> items);
+    ValueTask AddRange(params TDocument[] items);
+    ValueTask Update(Expression<Func<TDocument, bool>> filter, TDocument value);
+    ValueTask Remove(Expression<Func<TDocument, bool>> filter);
+    ValueTask RemoveRange(Expression<Func<TDocument, bool>> filter);
+    IQueryable<TDocument> AsQueryable();
+}
