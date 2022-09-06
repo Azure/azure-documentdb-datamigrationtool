d3cba9ac0c314337d0ba7e443ac51e58fe149d1d
John Bowen <john@bowencode.net>
2022-09-06 11:50:16 -0700

    Adding tests and usage docs for JSON extension

--- Extensions/Json/Microsoft.DataTransfer.JsonExtension.UnitTests/JsonSourceTests.cs
+++ Extensions/Json/Microsoft.DataTransfer.JsonExtension.UnitTests/JsonSourceTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.DataTransfer.Interfaces;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.DataTransfer.JsonExtension.UnitTests
@@ -19,6 +20,30 @@ namespace Microsoft.DataTransfer.JsonExtension.UnitTests
                 CollectionAssert.AreEquivalent(new[] { "id", "name" }, dataItem.GetFieldNames().ToArray());
                 Assert.IsNotNull(dataItem.GetValue("id"));
                 Assert.IsNotNull(dataItem.GetValue("name"));
+            }
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_WithNestedObjects_ReadsValues()
+        {
+            var extension = new JsonDataSourceExtension();
+            var config = TestHelpers.CreateConfig(new Dictionary<string, string>
+            {
+                { "FilePath", "Data/NestedObjects.json" }
+            });
+
+            await foreach (var dataItem in extension.ReadAsync(config))
+            {
+                if (dataItem.GetValue("child") is IDataItem child)
+                {
+                    CollectionAssert.AreEquivalent(new[] { "type", "data" }, child.GetFieldNames().ToArray());
+                    Assert.IsNotNull(child.GetValue("type"));
+                    Assert.IsNotNull(child.GetValue("data"));
+                }
+                else
+                {
+                    Assert.Fail();
+                }
             }
         }
     }
