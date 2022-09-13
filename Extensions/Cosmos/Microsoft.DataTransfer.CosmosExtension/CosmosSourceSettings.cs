5f63afeabf5689be0d425d183a8ad41360b7529a
John Bowen <john@bowencode.com>
2022-09-13 11:20:10 -0700

    Feature/cosmos source (#184)
    * Adding Cosmos source and improving nested array handling
    
    * Adding tests

---
+++ Extensions/Cosmos/Microsoft.DataTransfer.CosmosExtension/CosmosSourceSettings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.DataTransfer.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.DataTransfer.CosmosExtension
+{
+    public class CosmosSourceSettings : IDataExtensionSettings
+    {
+        [Required]
+        public string? ConnectionString { get; set; }
+        [Required]
+        public string? Database { get; set; }
+        [Required]
+        public string? Container { get; set; }
+
+        public string? PartitionKey { get; set; }
+
+        public string? Query { get; set; }
+
+        public bool IncludeMetadataFields { get; set; }
+    }
+}
