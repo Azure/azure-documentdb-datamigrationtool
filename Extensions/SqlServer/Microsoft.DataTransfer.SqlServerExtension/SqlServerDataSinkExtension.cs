597ebf2be294e6ac612466c287259de0963c4548
John Bowen <john@bowencode.net>
2022-09-16 15:40:28 -0700

    Adding basic outline of Sql Extension

---
+++ Extensions/SqlServer/Microsoft.DataTransfer.SqlServerExtension/SqlServerDataSinkExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.DataTransfer.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.DataTransfer.SqlServerExtension
+{
+    [Export(typeof(IDataSinkExtension))]
+    public class SqlServerDataSinkExtension : IDataSinkExtension
+    {
+        public string DisplayName => "SqlServer";
+
+        public async Task WriteAsync(IAsyncEnumerable<IDataItem> dataItems, IConfiguration config, CancellationToken cancellationToken = default)
+        {
+            var settings = config.Get<SqlServerSinkSettings>();
+            settings.Validate();
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ Extensions/SqlServer/Microsoft.DataTransfer.SqlServerExtension/SqlServerDataSourceExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+using Microsoft.DataTransfer.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.DataTransfer.SqlServerExtension
+{
+    [Export(typeof(IDataSourceExtension))]
+    public class SqlServerDataSourceExtension : IDataSourceExtension
+    {
+        public string DisplayName => "SqlServer";
+
+        public IAsyncEnumerable<IDataItem> ReadAsync(IConfiguration config, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var settings = config.Get<SqlServerSourceSettings>();
+            settings.Validate();
+
+            throw new NotImplementedException();
+        }
+    }
+}
